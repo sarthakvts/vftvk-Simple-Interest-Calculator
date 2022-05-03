be9c63aa6361cdb68deda520e8ad51531b3476a3
Sarthak Vats <sarthakvts@gmail.com>
2022-05-04 01:37:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var principal = document.getElementById("principal").value;
+   var rate = document.getElementById("rate").value;
+   var interest = principal * years * rate /100;
+   var year = new Date().getFullYear()+parseInt(years);
+   var lin1= `If you deposit ${principal}, \nat an interest rate of ${rate}. \nYou will receive an amount of ${interest}, \n in the year ${year}`;
+
+function updateResult(){
+    document.getElementById("result").innerText= lin1;
+}
+
 }
         
